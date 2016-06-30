4b3ba624f7c201606ea5edf3f2ff84670e8182ee
nmella <nmella@uc.cl>
2016-06-30 11:29:14 -0400

    Desinstalar Cart2Quote

--- app/code/community/Ophirah/Qquoteadv/Block/Renderers/Default.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Ophirah_Qquoteadv_Block_Renderers_Default extends Ophirah_Qquoteadv_Block_Renderers_Abstract
-{
-
-
-}
--- app/code/community/Ophirah/Qquoteadv/Block/Renderers/Grouped.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Ophirah_Qquoteadv_Block_Renderers_Grouped extends Ophirah_Qquoteadv_Block_Renderers_Abstract
-{
-
-
-}
